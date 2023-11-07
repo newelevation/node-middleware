da528cd8460b01bb9fffc32266f7a3708087f8b2
andreventuravale <andreventuravale@gmail.com>
2023-11-07 12:29:52 -0300

    .eslintrc.js | .gitignore | index.js | index.test.js | package.json | pnpm-lock.yaml |

---
+++ index.test.js
@@ -0,0 +1,46 @@
+
+const mid = ({ use = [] }) => {
+  return () => {
+    return async (input) => {
+      const list = use.slice(0)
+
+      let output
+
+      let keep = true
+
+      let current = list.shift()
+
+      const next = (input, newOutput) => {
+        output = newOutput
+
+        current = list.shift()
+
+        keep = !!current
+      }
+
+      while (keep) {
+        keep = false
+
+        await current(next)(input, output)
+      }
+
+      return output
+    }
+  }
+}
+
+test("", async () => {
+  const client = mid({
+    use: [
+      n => async (i, o) => { n(i, 'a') },
+      n => async (i, o) => { n(i, 'b') },
+      n => async (i, o) => { n(i, 'c') }
+    ]
+  })
+
+  const request = client()
+
+  const output = await request('z')
+
+  expect(output).toEqual('c')
+})
