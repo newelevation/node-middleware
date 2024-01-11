f468891544cd9fbe071e058ee5e419c616fcc785
avale-ne <andre.vale@newelevation.com>
2024-01-11 14:49:58 -0300

    build and bump

--- dist/middleware.d.ts
+++ dist/middleware.d.ts
@@ -1,14 +1,10 @@
-export type MiddlewareHandler<Input = any, Output = any> = (
-  input: Input,
-  output?: Output,
-) => Promise<Output>;
+export type MiddlewareHandler<Input = any, Output = any> = (input: Input, output?: Output) => Promise<Output>;
 export type Next = (input: any, output: any) => Promise<any>;
-export type Middleware<Input = any> = (next: Next) => MiddlewareHandler<Input>;
-export type Pipeline<Input = any> = <Output>() => MiddlewareHandler<
-  Input,
-  Output
->;
-export declare const makePipeline: <Input>(
-  use?: Middleware[],
-) => Pipeline<Input>;
+export type UnamedMiddleware<Input = any> = (next: Next) => MiddlewareHandler<Input>;
+export type NamedMiddleware = [string, UnamedMiddleware];
+export type Middleware = UnamedMiddleware | NamedMiddleware;
+export type InsertionPlacement = "before" | "after";
+export type Insertion = [InsertionPlacement, name: string, UnamedMiddleware];
+export type Pipeline<Input = any> = <Output>(insertions?: Insertion[]) => MiddlewareHandler<Input, Output>;
+export declare const makePipeline: <Input>(use?: Middleware[]) => Pipeline<Input>;
 export declare const passOutputAlong: Next;
